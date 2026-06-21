4564e9fd166af5c9898a18f6b2b757790e9048cb
gemakfy <gemakfy@gmail.com>
2026-06-21 20:14:23 +0300

    add pipewire rate

--- hardware/modules/pipewire.nix
+++ hardware/modules/pipewire.nix
@@ -5,5 +5,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [ 44100 48000 88200 96000 176400 192000 352800 384000 ];
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 8192;
+      };
+    };
   };
 }
